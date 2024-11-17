aec1900e565d25ee58cebed0466b11381437c76e
indigoidk <56944117+indigoidk@users.noreply.github.com>
2024-11-17 14:19:40 -0600

    Update PowerShell_Dell_Bios_AC_Recovery.ps1

--- PowerShell_Dell_Bios_AC_Recovery.ps1
+++ PowerShell_Dell_Bios_AC_Recovery.ps1
@@ -12,7 +12,7 @@ Install-Module -Name DellBIOSProvider
 #Check/Install Visual C++ Redist Depdendacy
 if (-not (Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall' | Get-ItemProperty | Where-Object { $_.DisplayName -match 'Microsoft Visual C\+\+.*Redistributable \(x64\)' })) { Invoke-WebRequest 'https://aka.ms/vs/17/release/vc_redist.x64.exe' -OutFile "$env:TEMP\vc_redist.x64.exe"; Start-Process -FilePath "$env:TEMP\vc_redist.x64.exe" -ArgumentList '/install', '/quiet', '/norestart' -Wait }#Trust Powershell Script
 #This is not signed .PS1 file; please use the next command with cautiuon
-Set-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy Unrestricted -Force
 #Load Dell Bios Powershell Module
 Import-Module "DellBIOSProvider"
 # Set Dell Bios setting to AC Recover to On, Block Sleep, DeepSleepCntrl.
